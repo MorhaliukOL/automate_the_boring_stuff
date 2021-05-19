272617b50a4024ebdc2d6bd830e44d5fbd5bf81f
Oleh <morhaliuk@gmail.com>
2021-05-19 13:45:27 +0300

    12 searchpypi added

---
+++ 12_web_scraping/searchpypi.py
@@ -0,0 +1,39 @@
+"""
+searchpypi.py - opens in the browser several search results 
+from https://pypi.org
+"""
+
+import sys
+import time
+import webbrowser
+import requests
+
+from bs4 import BeautifulSoup
+
+
+PYPI = 'https://pypi.org'
+SELECTOR = '.unstyled > li > a'
+# number of search results to open in new tab
+NUM_LINKS = 5
+
+search = ' '.join(sys.argv[1:]) 
+
+if not search:
+    print("Search query not found! Try again.")
+    sys.exit()
+print("Searching...")
+
+search_link = PYPI + '/search/?q=' + search
+
+pypi_response = requests.get(search_link)
+pypi_response.raise_for_status()
+
+pypi_soup = BeautifulSoup(pypi_response.text, 'html.parser')
+
+top_results = pypi_soup.select(SELECTOR)[:5]
+
+for res in top_results:
+    res_link = PYPI + res.get('href')
+    print("Opening ", res_link)
+    time.sleep(0.5)
+    webbrowser.open(res_link)
